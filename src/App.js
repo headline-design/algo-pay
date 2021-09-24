4fb7b74d7853982e8d8024465f0a0fd961444118
headline-design <85418370+headline-design@users.noreply.github.com>
2021-09-24 12:11:33 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
 import React, { Component } from 'react'
-
-import Chart from "react-apexcharts";
-
 import './App.css';
-
 import 'react-dropdown/style.css';
 import ChartData from './ChartData.js'
 
-
 import {
   Pipeline,
-  AsaList,
   AlgoButton,
   AlgoSendButton,
-  AlgoFetch,
   Button,
-  Heading,
   Card,
   AlgoAddress,
-  Field,
-  Input,
-  Select,
-  Blockie,
-  SwitchNet,
   Modal
 } from 'pipeline-ui'
 
 
 var indexerURL = "https://algoexplorerapi.io/idx2/v2/accounts/";
-var asaNames = AsaList;
 const myAlgoWallet = Pipeline.init();
-const opt = [
-  { value: 'Algorand', label: 'Algorand' },
-  { value: 'ASA', label: 'Verified ASA' },
-  { value: 'ASAindex', label: 'ASA from Index Number' }
-]
-class test extends Component {
+
+var index = localStorage.getItem("index");
+var amount = localStorage.getItem("amount");
+var note = localStorage.getItem("note");
+var recipient = localStorage.getItem("recipient");
+class AlgoPay extends Component {
 
   constructor(props) {
     super(props);
@@ -79,110 +65,35 @@ class test extends Component {
       });
   }
 
-  recipientChangeHandler = (event) => {
-    this.setState({ recipient: event.target.value });
-  }
-
-  amountChangeHandler = (event) => {
-    this.setState({ amount: event.target.value });
-  }
-
-  noteChangeHandler = (event) => {
-    this.setState({ note: event.target.value });
-  }
-
-  asaIndexChangeHandler = (event) => {
-    this.setState({ asaNumb: event.target.value });
-  }
-
-  asaChangeHandler = (event) => {
-    this.setState({ asa: event.value });
-    switch (event.value) {
-      case "Algorand":
-        this.setState({ asaNumbVis: "none", asaIndVis: "none" });
-        this.setState({ asaNumb: 0 });
-        break;
-      case "ASA":
-        this.setState({ asaNumbVis: "block", asaIndVis: "none" });
-        break;
-      case "ASAindex":
-        this.setState({ asaNumbVis: "none", asaIndVis: "block" });
-        break;
-    }
-  }
-
-  asaNumbChangeHandler = (event) => {
-    this.setState({ asaNumb: event.value });
-
-  }
-
 
   render() {
     return <div align="center">
-
-      <Heading>Pipeline UI Demo</Heading>
       <Button onClick={() => this.setState({ isOpen: true })}>Buy it now!</Button>
       <Modal isOpen={this.state.isOpen} >
-        <Card width="500">
+        <Card width="350">
           <div align="right">
-          <Button.Outline size="small" onClick={() => this.setState({ isOpen: false })}>Close</Button.Outline>
+            <Button.Outline size="small" onClick={() => this.setState({ isOpen: false })}>Close</Button.Outline>
           </div>
+          <div align="center">
           <AlgoButton wallet={myAlgoWallet} context={this} returnTo={"address"} />
 
           <Card bg="blue" color="white" maxWidth={"500px"}>{this.state.con_status_text + this.state.balance}<br></br><Button onClick={this.updateBalance}>Refresh</Button></Card>
 
-          <AlgoAddress maxWidth={"500px"} address={this.state.address} textLabels /><br></br>
-
-          <table>
-            <tr>
-              <td><Field label="Select your asset:"></Field>
-                <div style={{ maxWidth: '500px' }}>
-                  <Select
-                    style={{ maxWidth: '350px' }}
-                    defaultValue={this.state.value}
-                    onChange={this.asaChangeHandler}
-                    options={opt}
-                  />
-                </div>
-                <div style={{ maxWidth: '500px', display: this.state.asaNumbVis }}>
-
-
-                  <Field label="Verified ASA's:" required={true} ></Field>
-                  <Select
-                    defaultValue={this.state.value}
-                    onChange={this.asaChangeHandler}
-                    options={asaNames} onChange={this.asaNumbChangeHandler} /></div>
-
-                <div style={{ maxWidth: '500px', display: this.state.asaIndVis }}>
-                  <Field label="ASA Index Number:" style={{ maxWidth: '500px' }}>
-                    <Input style={{ maxWidth: '500px' }} type="text" required={true} placeholder="" onChange={this.asaIndexChangeHandler} />
-                  </Field><br></br>
-                </div>
-
-                <Field label="Recipient Address" style={{ maxWidth: '500px' }}>
-                  <Input style={{ maxWidth: '500px' }} type="text" required={true} placeholder="" onChange={this.recipientChangeHandler} />
-                </Field><br></br>
-                <Field style={{ maxWidth: '500px' }} label="Amount (in micro Algos)">
-                  <Input style={{ maxWidth: '500px' }} type="number" required={true} placeholder="" onChange={this.amountChangeHandler} />
-                </Field><br></br>
-                <Field style={{ maxWidth: '500px' }} label="Note">
-                  <Input style={{ maxWidth: '500px' }} type="text" required={true} placeholder="" onChange={this.noteChangeHandler} />
-                </Field><br></br></td>
-              <td><AlgoSendButton
-                index={this.state.asaNumb}
-                recipient={this.state.recipient}
-                amount={this.state.amount}
-                note={this.state.note}
-                myAddress={this.state.address}
-                wallet={myAlgoWallet}
-                context={this}
-                returnTo={"txID"}
-              /><br></br>
-                <Card bg="blue" color="white" maxWidth={"500px"}>
-                  {"Transaction ID: " + this.state.txID}
-                </Card></td>
-            </tr>
-          </table>
+          <AlgoAddress maxWidth={"350px"} address={this.state.address} textLabels /><br></br>
+            <AlgoSendButton
+              index={index}
+              recipient={recipient}
+              amount={amount}
+              note={note}
+              myAddress={this.state.address}
+              wallet={myAlgoWallet}
+              context={this}
+              returnTo={"txID"}
+            /><br></br>
+            <Card bg="blue" color="white" maxWidth={"500px"}>
+              {"Transaction ID: " + this.state.txID}
+            </Card>
+          </div>
 
         </Card>
       </Modal>
@@ -190,19 +101,4 @@ class test extends Component {
   }
 }
 
-/* place below code between div tags to include chart
- 
-<div className="app">
-       <div className="row">
-         <div className="mixed-chart">
-           <Chart
-             options={this.state.ChartData.options}
-             series={this.state.ChartData.series}
-             type="candlestick"
-             width="500"
-           />
-         </div>
-       </div>
-     </div> */
-
-export default test
+export default AlgoPay;
