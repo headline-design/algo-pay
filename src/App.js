8742173bcb50a82dbc3022c5b37169208ac2f16e
headline-design <85418370+headline-design@users.noreply.github.com>
2021-09-24 13:18:56 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import {
   Button,
   Card,
   AlgoAddress,
-  Modal
+  Modal,
+  Link
 } from 'pipeline-ui'
 
 
@@ -29,7 +30,7 @@ class AlgoPay extends Component {
       update: false,
       asaIndVis: "none",
       myTransactions: ["1"],
-      tableVis: "none",
+      tableVis: false,
       balance: "",
       asaNumbVis: "none",
       asa: "Algorand",
@@ -41,14 +42,15 @@ class AlgoPay extends Component {
       con_status_text: "Status: Not Connected",
       address: "",
       ChartData,
-      isOpen: false
+      isOpen: false,
+      completed: false
     };
   }
 
   componentDidUpdate(_prevProps, prevState) {
     if (prevState.address !== this.state.address) {
       this.setState({ con_status_text: "Connected" });
-      this.setState({ tableVis: "block" });
+      this.setState({ tableVis: true });
       this.updateBalance();
     }
   }
@@ -65,6 +67,10 @@ class AlgoPay extends Component {
       });
   }
 
+  componentDidMount() {
+    this.interval = setInterval(() => { if (this.state.txID != "") { this.setState({ completed: true }) } }, 1000);
+  }
+
 
   render() {
     return <div align="center">
@@ -75,24 +81,30 @@ class AlgoPay extends Component {
             <Button.Outline size="small" onClick={() => this.setState({ isOpen: false })}>Close</Button.Outline>
           </div>
           <div align="center">
-          <AlgoButton wallet={myAlgoWallet} context={this} returnTo={"address"} />
+            <AlgoButton wallet={myAlgoWallet} context={this} returnTo={"address"} />
 
-          <Card bg="blue" color="white" maxWidth={"500px"}>{this.state.con_status_text + this.state.balance}<br></br><Button onClick={this.updateBalance}>Refresh</Button></Card>
+            {this.state.tableVis ? <div>
+              <Card bg="blue" color="white" maxWidth={"500px"}>{this.state.con_status_text + this.state.balance}<br></br>
+                <Button onClick={this.updateBalance}>Refresh</Button></Card>
+              <AlgoAddress maxWidth={"350px"} address={this.state.address} textLabels /><br></br>
+              <AlgoSendButton
+                index={index}
+                recipient={recipient}
+                amount={amount}
+                note={note}
+                myAddress={this.state.address}
+                wallet={myAlgoWallet}
+                context={this}
+                returnTo={"txID"}
+              /><br></br>
+            </div>
+              : null}
+            {this.state.completed ? <Card bg="grey" color="white" maxWidth={"500px"}>
+              <Link href={"https://algoexplorer.io/tx/" + this.state.txID} target="_blank" title="View in AlgoExplorer (5 sec delay)">
+                View in AlgoExplorer (5 sec delay)
+              </Link>
+            </Card> : null}
 
-          <AlgoAddress maxWidth={"350px"} address={this.state.address} textLabels /><br></br>
-            <AlgoSendButton
-              index={index}
-              recipient={recipient}
-              amount={amount}
-              note={note}
-              myAddress={this.state.address}
-              wallet={myAlgoWallet}
-              context={this}
-              returnTo={"txID"}
-            /><br></br>
-            <Card bg="blue" color="white" maxWidth={"500px"}>
-              {"Transaction ID: " + this.state.txID}
-            </Card>
           </div>
 
         </Card>
